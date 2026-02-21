ae0284dd060dcd95ffed9a5a6d9601621a4684f3
Harsh-Tripathi-231103 <harsh2454@gmail.com>
2026-02-21 18:48:30 +0530

    chore: remove generated artifacts and ignore local files

--- backend/dist/utils/jwt.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-import { env } from "../config/env.js";
-export function signToken(payload) {
-    return jwt.sign(payload, env.JWT_SECRET, { expiresIn: "30d" });
-}
-export function verifyToken(token) {
-    return jwt.verify(token, env.JWT_SECRET);
-}
